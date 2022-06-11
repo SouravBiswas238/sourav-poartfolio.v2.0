bd537d4ec523ecae03e9110532332ee3ee044839
SouravBiswas238 <souravbiswas427@gmail.com>
2022-06-11 15:24:21 +0600

    fixing bug

--- src/components/About/Progress.js
+++ src/components/About/Progress.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Progress = ({ value, name }) => {
     return (
         <div className='text-center'>
-            <div class="radial-progress text-warning" style={{ '--value': value }}>{value}%</div>
+            <div class="radial-progress text-warning border-2" style={{ '--value': value }}>{value}%</div>
             <h2 className='my-2 text-center uppercase text-white'>{name}</h2>
         </div>
     );
